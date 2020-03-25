6213d5abb8696c25573df7e20a48d5be87c8d20f
heihuxiaoyue <heihuxaoyue@163.com>
2020-03-25 18:23:48 +0800

    generate T data

--- stock_data.py
+++ stock_data.py
@@ -125,27 +125,30 @@ def generate_train_data():
         cursor.execute("select ts_code,symbol,name,area,industry,fullname,enname,market,exchange,curr_type,list_status,list_date,delist_date,is_hs from `stock_basic` where ts_code='{}' ".format(ts_code))
         # trade_date = (datetime.datetime.now() - datetime.timedelta(days=2)).strftime('%Y%m%d')
         # cursor.execute("select * from daily where ts_code='{}' and trade_date='{}'".format(ts_code, trade_date))
-        daily_data = cursor.fetchall()
-        for single_row in daily_data:
-            common_row.append(single_row[0])
-            common_row.append(single_row[1])
-            common_row.append(single_row[2])
-            common_row.append(single_row[3])
-            common_row.append(single_row[4])
-            common_row.append(single_row[5])
-            common_row.append(single_row[6])
-            common_row.append(single_row[7])
-            common_row.append(single_row[8])
-            common_row.append(single_row[9])
-            common_row.append(single_row[10])
-            common_row.append(single_row[11])
-            common_row.append(single_row[12])
-            common_row.append(single_row[13])
+        stock_basic = cursor.fetchall()
+        if stock_basic:
+            common_row.append(stock_basic[0][0])
+            common_row.append(stock_basic[0][1])
+            common_row.append(stock_basic[0][2])
+            common_row.append(stock_basic[0][3])
+            common_row.append(stock_basic[0][4])
+            common_row.append(stock_basic[0][5])
+            common_row.append(stock_basic[0][6])
+            common_row.append(stock_basic[0][7])
+            common_row.append(stock_basic[0][8])
+            common_row.append(stock_basic[0][9])
+            common_row.append(stock_basic[0][10])
+            common_row.append(stock_basic[0][11])
+            common_row.append(stock_basic[0][12])
+            common_row.append(stock_basic[0][13])
+        else:
+            logging.info("[{}] 获取stock_basic值为空: {}".format(loopCount, ts_code))
+            continue
         for i in range(2, 2000):
             skip = False
             cur_row = []
             cur_row.extend(common_row)
-            for j in range(0, 10):
+            for j in range(0, 200):
                 shift = i+j
                 trade_date = (datetime.datetime.now() - datetime.timedelta(days=shift)).strftime('%Y%m%d')
                 cursor.execute("select exchange,cal_date,is_open from `trade_cal` where cal_date='{}' ".format(trade_date))
@@ -156,8 +159,8 @@ def generate_train_data():
                     cur_row.append(trade_cal[0][2])
                 else:
                     skip = True
-                    logging.debug("[{}] cal_date: {} {}".format(loopCount, ts_code, trade_date))
-            for j in range(0, 10):
+                    logging.info("[{}] cal_date: {} {}".format(loopCount, ts_code, trade_date))
+            for j in range(0, 200):
                 shift = i+j
                 trade_date = (datetime.datetime.now() - datetime.timedelta(days=shift)).strftime('%Y%m%d')
                 cursor.execute("select `ts_code`,`trade_date`,`open`,`high`,`low`,`close`,`pre_close`,`change`,`pct_chg`,`vol`,`amount` from daily where ts_code='{}' and trade_date='{}'".format(ts_code, trade_date))
@@ -207,6 +210,152 @@ def generate_train_data():
                 train_row.to_csv(output_data_dir+"train.csv", sep=',', index=False, mode='a', header=False)
         logging.info("[{}] finished: {}".format(loopCount, ts_code))
 
+def generate_predict_data():
+    train_data_header = True
+    for ts_code in stock_pool:
+        common_row = []
+        cursor.execute("select ts_code,symbol,name,area,industry,fullname,enname,market,exchange,curr_type,list_status,list_date,delist_date,is_hs from `stock_basic` where ts_code='{}' ".format(ts_code))
+        # trade_date = (datetime.datetime.now() - datetime.timedelta(days=2)).strftime('%Y%m%d')
+        # cursor.execute("select * from daily where ts_code='{}' and trade_date='{}'".format(ts_code, trade_date))
+        stock_basic = cursor.fetchall()
+        if stock_basic:
+            common_row.append(stock_basic[0][0])
+            common_row.append(stock_basic[0][1])
+            common_row.append(stock_basic[0][2])
+            common_row.append(stock_basic[0][3])
+            common_row.append(stock_basic[0][4])
+            common_row.append(stock_basic[0][5])
+            common_row.append(stock_basic[0][6])
+            common_row.append(stock_basic[0][7])
+            common_row.append(stock_basic[0][8])
+            common_row.append(stock_basic[0][9])
+            common_row.append(stock_basic[0][10])
+            common_row.append(stock_basic[0][11])
+            common_row.append(stock_basic[0][12])
+            common_row.append(stock_basic[0][13])
+        else:
+            logging.info("[{}] 获取stock_basic值为空: {}".format(loopCount, ts_code))
+            continue
+        for i in range(1, 2):
+            skip = False
+            cur_row = []
+            cur_row.extend(common_row)
+            for j in range(0, 200):
+                shift = i+j
+                trade_date = (datetime.datetime.now() - datetime.timedelta(days=shift)).strftime('%Y%m%d')
+                cursor.execute("select exchange,cal_date,is_open from `trade_cal` where cal_date='{}' ".format(trade_date))
+                trade_cal = cursor.fetchall()
+                if trade_cal:
+                    cur_row.append(trade_cal[0][0])
+                    cur_row.append(trade_cal[0][1])
+                    cur_row.append(trade_cal[0][2])
+                else:
+                    skip = True
+                    logging.info("[{}] cal_date: {} {}".format(loopCount, ts_code, trade_date))
+            for j in range(0, 200):
+                shift = i+j
+                trade_date = (datetime.datetime.now() - datetime.timedelta(days=shift)).strftime('%Y%m%d')
+                cursor.execute("select `ts_code`,`trade_date`,`open`,`high`,`low`,`close`,`pre_close`,`change`,`pct_chg`,`vol`,`amount` from daily where ts_code='{}' and trade_date='{}'".format(ts_code, trade_date))
+                daily = cursor.fetchall()
+                if daily:
+                    cur_row.append(daily[0][1])
+                    cur_row.append(daily[0][2])
+                    cur_row.append(daily[0][3])
+                    cur_row.append(daily[0][4])
+                    cur_row.append(daily[0][5])
+                    cur_row.append(daily[0][6])
+                    cur_row.append(daily[0][7])
+                    cur_row.append(daily[0][8])
+                    cur_row.append(daily[0][9])
+                    cur_row.append(daily[0][10])
+                else:
+                    # skip = True
+                    cur_row.append('')
+                    cur_row.append(0.0)
+                    cur_row.append(0.0)
+                    cur_row.append(0.0)
+                    cur_row.append(0.0)
+                    cur_row.append(0.0)
+                    cur_row.append(0.0)
+                    cur_row.append(0.0)
+                    cur_row.append(0.0)
+                    cur_row.append(0.0)
+                    logging.debug("[{}] 获取历史daily值为空: {} {}".format(loopCount, ts_code, trade_date))
+            if skip:
+                continue
+            train_row = pd.DataFrame(np.expand_dims(cur_row, axis=0))
+            if train_data_header:
+                train_row.to_csv(output_data_dir+"predict.csv", sep=',', index=False, mode='w', header=True)
+                train_data_header = False
+            else:
+                train_row.to_csv(output_data_dir+"predict.csv", sep=',', index=False, mode='a', header=False)
+        logging.info("[{}] finished: {}".format(loopCount, ts_code))
+
+
+def generate_T_data():
+    #用前面几天的数据预测
+    prepare_len = 11
+    empty_csv = True
+    for ts_code in stock_pool:
+        df = pro.daily(ts_code=ts_code)
+        c_len = df.shape[0]
+        print('generate_T_data: ', ts_code, c_len)
+
+        for i in range(c_len):
+            cur_row = []
+            if i + prepare_len < c_len:
+                cur_row.append(df.loc[i]['low'])
+                cur_row.append(df.loc[i]['high'])
+                for j in range(1, prepare_len+1):
+                    index = i + j
+                    cur_row.append(df.loc[index]['ts_code'])
+                    cur_row.append(df.loc[index]['trade_date'])
+                    cur_row.append(df.loc[index]['open'])
+                    cur_row.append(df.loc[index]['high'])
+                    cur_row.append(df.loc[index]['low'])
+                    cur_row.append(df.loc[index]['close'])
+                    cur_row.append(df.loc[index]['pre_close'])
+                    cur_row.append(df.loc[index]['change'])
+                    cur_row.append(df.loc[index]['pct_chg'])
+                    cur_row.append(df.loc[index]['vol'])
+                    cur_row.append(df.loc[index]['amount'])
+                train_row = pd.DataFrame(np.expand_dims(cur_row, axis=0))
+                if empty_csv:
+                    train_row.to_csv(output_data_dir+"train.csv", sep=',', index=False, mode='w', header=True)
+                    empty_csv = False
+                else:
+                    train_row.to_csv(output_data_dir+"train.csv", sep=',', index=False, mode='a', header=False)
+
+    empty_csv = True
+    for ts_code in stock_pool:
+        df = pro.daily(ts_code=ts_code)
+        c_len = df.shape[0]
+        print('generate_T_data predict: ', ts_code, c_len)
+
+        cur_row = []
+        if prepare_len < c_len:
+            for j in range(1, prepare_len+1):
+                index = j
+                cur_row.append(df.loc[index]['ts_code'])
+                cur_row.append(df.loc[index]['trade_date'])
+                cur_row.append(df.loc[index]['open'])
+                cur_row.append(df.loc[index]['high'])
+                cur_row.append(df.loc[index]['low'])
+                cur_row.append(df.loc[index]['close'])
+                cur_row.append(df.loc[index]['pre_close'])
+                cur_row.append(df.loc[index]['change'])
+                cur_row.append(df.loc[index]['pct_chg'])
+                cur_row.append(df.loc[index]['vol'])
+                cur_row.append(df.loc[index]['amount'])
+            train_row = pd.DataFrame(np.expand_dims(cur_row, axis=0))
+            if empty_csv:
+                train_row.to_csv(output_data_dir+"predict.csv", sep=',', index=False, mode='w', header=True)
+                empty_csv = False
+            else:
+                train_row.to_csv(output_data_dir+"predict.csv", sep=',', index=False, mode='a', header=False)
+    print('generate_T_data Finished!')
+
+
 # 设定获取日线行情的初始日期和终止日期，其中终止日期设定为昨天。
 start_dt = '20200206'
 time_temp = datetime.datetime.now() - datetime.timedelta(days=10)
@@ -235,7 +384,8 @@ try:
     while loopCount < maxLoopCount:
         try:
             # download_stock_data()
-            generate_train_data()
+            # generate_train_data()
+            generate_T_data()
 
 
             #     daily = pd.concat([daily,daily1],ignore_index=True)
